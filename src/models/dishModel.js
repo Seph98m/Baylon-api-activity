8fe5c088472478b7b9ab7d58f22e2628047435e1
Seph98m <baylon.markjoseph@marsu.edu.ph>
2026-01-29 11:25:36 +0800

    Complete Dishes API project

---
+++ src/models/dishModel.js
@@ -0,0 +1,32 @@
+const dishes = [
+    {
+        id:1,
+        name: 'Spicy Burger',
+        price: 12.99,
+        category:'lunch',
+        isVegetarian: 'false',
+    },
+    {
+        id:2,
+        name: 'Ceasar Salad',
+        price: 8.5,
+        category:'starter',
+        isVegetarian: 'true',
+    },
+    {
+        id:3,
+        name: 'Chocolate Lava Cake',
+        price: 6.0,
+        category:'dessert',
+        isVegetarian: 'true',
+    },
+    {
+        id:4,
+        name: 'Grilled Salmon',
+        price: 18.0,
+        category:'dinner',
+        isVegetarian: 'false',
+    },
+];
+
+module.exports = dishes;
