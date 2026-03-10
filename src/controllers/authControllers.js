7fe7ed62ec30c86acde54ff584f877ebf766ce7b
Seph <baylon.markjoseph@marsu.edu.ph>
2026-03-10 13:10:15 +0800

    l

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -17,8 +17,7 @@ expiresIn: '30d' // Token expires in 30 days
  // Check if user exists
  const userExists = await User.findOne({ email });
  if (userExists) return res.status(400).json({ message: 'User already exists' });
-
-s // Create user
+ 
 const user = await User.create({ name, email, password, role });
 
  res.status(201).json({
